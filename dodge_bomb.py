5dee7eba6a345ce0bbfcacc7b459f6a812b8fff5
Ryota <c0c240552b@edu.teu.ac.jp>
2025-11-25 14:19:26 +0900

    練習問題１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,10 +10,18 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")     
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    
+    DELTA = {
+        pg.K_UP: (0, -5),
+        pg.K_DOWN: (0, +5),
+        pg.K_LEFT: (-5, 0),
+        pg.K_RIGHT: (+5, 0)
+    }
+    
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -24,15 +32,18 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+        
         for k, v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+                
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
+        
 
 
 if __name__ == "__main__":
