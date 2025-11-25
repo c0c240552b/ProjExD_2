f48d3a33bad8ca41cdfa4c73097d46a541a6ed2f
Ryota <c0c240552b@edu.teu.ac.jp>
2025-11-25 14:20:51 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random # 乱数を使うためインポート
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -14,6 +15,20 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    
+    # 練習問題2: 爆弾の初期設定
+    # 爆弾 Surface の作成と透過処理
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    
+    # 爆弾 Rect の作成とランダム配置
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    
+    # 爆弾の速度設定
+    vx, vy = +5, +5 # 爆弾は一定速度で動く
     
     DELTA = {
         pg.K_UP: (0, -5),
@@ -40,10 +55,14 @@ def main():
                 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        
+        #練習問題2: 爆弾の移動と描画
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
-        
 
 
 if __name__ == "__main__":
